db628e64809b2b330630458f73ae532507b35282
Fiolka <fabiolajantura@gmail.com>
2019-04-09 21:13:06 +0200

    solved task

---
+++ program.js
@@ -0,0 +1,21 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch(instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case 'process.env': 
+				console.log(process.env);
+				break;
+			case 'process.versions': 
+				console.log(process.versions);
+				break;
+			default: 
+				process.stdout.write('Wrong instruction!\n');
+		}	
+	} 
+});
